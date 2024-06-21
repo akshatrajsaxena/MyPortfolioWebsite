23b880a53905f491598393f7e8f716f419923ff1
Akshat Raj Saxena <119042958+akshatrajsaxena@users.noreply.github.com>
2024-06-21 17:21:38 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: '/MyPortfolioWebsite/'
+  base: '/MyPortfolioWebsite/',
   css: {
     modules: {
       localsConvention: "camelCase",
